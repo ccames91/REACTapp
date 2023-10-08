22acd0d32200c0773556e6c40cd0a3312e4cf0d1
chandler ames <eddiehawk1991@gmail.com>
2023-10-08 10:39:12 -0700

    bio page display html

--- my-react-bio/src/index.js
+++ my-react-bio/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChandlerBio /> {/* Replace App with ChandlerBio */}
   </React.StrictMode>
 );
 
