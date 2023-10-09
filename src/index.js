c457361ba09a5e4f2351a35ef4aa83f5ad2fd9a5
chandler ames <eddiehawk1991@gmail.com>
2023-10-08 23:52:47 -0700

    file name change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Project2 from './Project-2'; // Import the Project2 component
+import Project2 from './project2'; // Updated import name
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Project-2 /> {/* Render the Project2 component */}
+    <Project2 /> {/* Render the Project2 component */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ... (rest of your code)
+
+
